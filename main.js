2c81ac78cac5a43fa0490b0b22463d42fc18bad2
Naihil Jun <naihil@gmail.com>
2018-12-25 09:47:34 +0900

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("New Project test naihil.com\n");
+  response.end("New Project test test naihil.com\n");
 });
 
 // listen on localhost:8000
